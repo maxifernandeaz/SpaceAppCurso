4b67316608ed4801f269758b88a112b1672e5b8a
maxifernandeaz <maximilianofernandezbaez@gmail.com>
2024-10-24 00:27:47 -0300

    proyecto finalizado

--- ejemplos/callback.js
+++ ejemplos/callback.js
@@ -1,31 +1,55 @@
 function uno (dos) {
     console.log("uno")
     setTimeout(dos, 2000)
+    
   
 }
 
 function dos (tres) {
     console.log("dos")
     setTimeout(tres,2000)
+    
 }
 
 function tres (cuatro) {
-    console.log("tres")
-    setTimeout(cuatro,2000)
-
+    console.log("tres");
+    setTimeout(cuatro, 2000)
+    
 }
 
-function cuatro (cinco) {
-    console.log("cuatro")
-    setTimeout(cinco,2000)
+// function tres (cuatro) {
+//     console.log("tres")
+//     setTimeout(cuatro,2000)
 
+// }
+
+// function cuatro (cinco) {
+//     console.log("cuatro")
+//     setTimeout(cinco,2000)
+
+// }
+
+// function cinco() {
+//     console.log("cinco")   
+// } 
+
+setTimeout(()=> uno(dos),5000)
+
+
+const nombre = function () {
+    console.log("nombre");
+    
 }
+nombre()
 
-function cinco() {
-    console.log("cinco")   
-} 
+const persona = ()=> console.log("Maxi");
 
+persona();
+ 
+() => console.log();
 
+ 
+/*
 setTimeout(() => uno(dos),5000)
 
 //calback hell
@@ -52,7 +76,7 @@ persona();
 
 
 
-
+*/
 
 
 
--- script.js
+++ script.js
@@ -1,57 +1,54 @@
 
-const url = "https://api.nasa.gov/planetary/apod?api_key=Md6ZhB8e1s2GDDDn6dEI4WialPr963Fl4DVh6PcA&count=15" 
- 
+const url = "https://api.nasa.gov/planetary/apod?api_key=4QyjP5pRPqzWp8d64KKnDYKw6teRg6S3hwzt9CjO&count=15";
 
 async function listaImagenes() {
-
-    try{
-        let fetchImagen = await fetch(url)
-        let datosImagenes = await fetchImagen.json()
-
-        console.log(datosImagenes)
-
-        const card = document.querySelector("[data-ul]")
-
-        datosImagenes.forEach( elemento => {
-
-            const contenido =
-            `<li class="card">
-                <img class="card__image" src="${elemento.url}" alt="imagen">
-                <h3 class="card__title">${elemento.title}</h3>
-            </li>
-            `
-
-            card.innerHTML = card.innerHTML + contenido
-        })
+    try {
+        let fetchImagenes = await fetch(url);
+        let datosImagenes = await fetchImagenes.json();
+        console.log(datosImagenes);
         
-    }
-    catch(error){
-        console.log(error)
+        const card = document.querySelector("[data-ul]"); // Asegúrate de que este selector sea correcto
+
+        datosImagenes.forEach(elemento => {
+            const contenido = `<li class="card">
+                                 <img class="card__image" src="${elemento.url}" alt="${elemento.title}">
+                                 <h3 class="card__title">${elemento.title}</h3>
+                             </li>`;
+            
+            card.insertAdjacentHTML('beforeend', contenido);
+        });
+        
+    } catch (error) {
+        console.log(error);
     }
 }
 
-listaImagenes()
+listaImagenes();
 
 
-// then y catch
-/* function listaImagenes(){
-    fetch(url)
-    .then( response => response.json())
-    .then( datosImagenes => {
-        console.log(datosImagenes)
 
-        const card = document.querySelector("[data-ul]")
 
-        datosImagenes.forEach( elemento => {
-            const contenido =`<li class="card">
-            <img class="card__image" src="${elemento.url}" alt="imagen">
-            <h3 class="card__title">${elemento.title}</h3>
-        </li>
-            `
-            card.innerHTML = card.innerHTML + contenido
-        })
-    })
-    .catch( error => console.log(error))
-}
 
-listaImagenes() */
+// function listaImagenes(){
+//     fetch (url)
+// .then( Response => Response.json())
+// .then( datosImagenes => {
+//     console.log(datosImagenes);
+
+//     const card = document.querySelector("[data-ul]")
+
+//     datosImagenes.forEach(Elemento => {
+//         const contenido = `<li class="card">
+//                     <img class="card__image" src="${Elemento.url}" alt="imagen">
+//                     <h3 class="card__title">${Elemento.title}</h3>
+//                 </li>`
+
+//                 card.innerHTML = card.innerHTML + contenido
+//     })
+    
+// })
+// .catch(error => console.log(error))
+
+// }
+
+// listaImagenes()
